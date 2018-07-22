0773751114f64213d3ff89385762ea01dcc4fa8c
PPShang <15271327342@163.com>
2018-07-22 20:26:53 +0800

    feat[hello]:新增模块hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+    -> File Name:        hello.cpp
+    -> Author:           shangpei
+    -> Mail:             15271327342@163.com 
+    -> Created Time:     Sun 22 Jul 2018 08:25:32 PM CST
+    -> File Description: 
+ ************************************************************************/
+
+#include <iostream>
+
+#include <boost/core/ignore_unused.hpp>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    boost::ignore_unused(argc, argv);
+    cout << "Hello World!" << endl;
+    return 0;
+}
